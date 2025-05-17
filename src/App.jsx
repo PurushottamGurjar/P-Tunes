d2d83e27f8a852a0ccc86ddaac5a993cf9e4dbf0
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-05-17 16:24:38 +0530

    Assets setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./app.css"
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div className="fullcontent">
+        
+        <div className="leftbar"></div>
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./navbar.css"
+import "../assets"
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='navbar'>
+        <img src="/logo.svg" alt="spotify" />
+
+        </div>
+      
+    </>
+  )
+}
+
+export default Navbar;
